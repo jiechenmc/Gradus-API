0de7e6b236ea96d7f8b46e64b4c7aae77f3b2994
Jie Chen <jiechenmc@gmail.com>
2022-11-21 18:05:02 -0500

    update default value on query

--- app/core/query.py
+++ app/core/query.py
@@ -2,7 +2,7 @@ from fastapi import Query
 
 QUERY = Query(default=...)
 
-FIELD = Query(default="Section",
+FIELD = Query(default="section",
               regex=r"^(section|term|courseTitle|instructor)$")
 
 TERM = Query(default="Spring 2022",
